a619471031f5f752f4955b16c412ff16fb37de87
a7000056 <a7000056@gmail.com>
2017-07-10 02:07:28 +0800

    新增DEMO程式

---
+++ OptimizationNoteWeb/OptimizationNoteWeb/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OptimizationNote.Data.DBContext;
+
+namespace OptimizationNoteWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new Ken_DemoEntities();
+            var query=db.PUBGOptimization.ToList();
+
+            return View(query);
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int serialNo)
+        {
+            var db = new Ken_DemoEntities();
+            var query = db.PUBGOptimization.FirstOrDefault(o=>o.SerialNo==serialNo);
+
+            return View(query);
+        }
+
+        public ActionResult Delete(int serialNo)
+        {
+            var result = true;
+
+            try
+            {
+                var db = new Ken_DemoEntities();
+                var query = db.PUBGOptimization.FirstOrDefault(o => o.SerialNo == serialNo);
+
+                db.PUBGOptimization.Remove(query);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return Json(new { success = result });
+        }
+
+        public ActionResult Add(PUBGOptimization model)
+        {
+            var result = true;
+
+            try
+            {
+                var db = new Ken_DemoEntities();
+
+                db.PUBGOptimization.Add(model);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            
+            return Json(new { success = result });
+        }
+
+        public ActionResult Update(PUBGOptimization model)
+        {
+            var result = true;
+
+            try
+            {
+                var db = new Ken_DemoEntities();
+                var query = db.PUBGOptimization.FirstOrDefault(o => o.SerialNo == model.SerialNo);
+                
+                query.DownloadURL = model.DownloadURL;
+                query.Description = model.Description;
+
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+
+            return Json(new { success = result });
+        }
+    }
+}
---
+++ OptimizationNoteWeb/OptimizationNoteWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OptimizationNoteWeb.MvcApplication" Language="C#" %>
